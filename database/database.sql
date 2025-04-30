3408c6042a096e67af8c94ac6eb88cf37c1e7ee9
juli6464 <juli6464@hotmail.com>
2025-04-29 20:33:11 -0500

    Feature: Insert wallets

--- database/database.sql
+++ database/database.sql
@@ -16,10 +16,19 @@ CREATE TABLE wallets (
   address VARCHAR(255) NOT NULL UNIQUE
 );
 
+-- Datos para la tabla users
 INSERT INTO users (name,email,password)
     VALUES ('Juan','juan@mail.com','$2a$12$xs/5mJ5W4tt35hu9xVfGL.rq9yEqqeavfNtKanhdFWUQXDesrKwue'),   --123456
            ('Pedro','pedro@mail.com','$2a$12$vQj7GaQ4WBQmBPaIRRU9quaBpiAskHcUYYbsCT6plZq9ukQJdhW7q'); --1234ab
 
+-- Datos para la tabla wallets
+INSERT INTO wallets (user_id, tag, chain, address)
+  VALUES 
+    (1, 'Wallet principal', 'Ethereum', '0xAbc1234567890abcdefABCDEF1234567890abcd'),
+    (1, 'Secundaria', 'Polygon', '0x1111222233334444555566667777888899990000'),
+    (2, 'Inversión', 'Binance Smart Chain', '0x9999888877776666555544443333222211110000'),
+    (2, 'Cold Storage', 'Bitcoin', '1BoatSLRHtKNngkdXEeobR76b53LETtpyT');
+    
 --Some commands in SQL shell
 -- \l  lista base de datos
 -- \c typescriptdatabase;   selecciona tabla
